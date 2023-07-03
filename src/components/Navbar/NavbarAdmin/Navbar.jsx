dfe7b513fc3d90ddc98d9cada6ed345fc374d2eb
farelyudapratama <98510749+farelyudapratama@users.noreply.github.com>
2023-07-03 17:18:10 +0700

    Update Navbar.jsx

--- src/components/Navbar/NavbarAdmin/Navbar.jsx
+++ src/components/Navbar/NavbarAdmin/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { logo, adminPhoto } from "../../../assets";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
